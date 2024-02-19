47aaecc7fbe8ff705a7720616a994fb3e19d5b1c
Angelo <a.allaham@student.saedubai.com>
2024-02-19 23:50:07 +0400

    Added scene with basic camera swapping

---
+++ Assets/_Project/Scripts/Camera/CameraSwap.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSwap : MonoBehaviour
+{
+    [SerializeField] private GameObject camera2D, camera3D;
+
+    void Update()
+    {
+      if(Input.GetKeyDown(KeyCode.Space))
+      {
+        CameraState();
+      }
+    }
+
+    private void CameraState()
+    {
+      if(Mode.mode3D)
+      {
+        camera3D.SetActive(true);
+        camera2D.SetActive(false);
+      } else {
+        camera2D.SetActive(true);
+        camera3D.SetActive(false);
+      }
+    }
+}
